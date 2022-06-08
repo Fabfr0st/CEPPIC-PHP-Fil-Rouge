ba20f6ab0ac68fcc58b562815d51f9bc594dde81
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-06-08 10:14:31 +0200

    removing html form from inscription

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -1,1 +1,11 @@
 <h1>Inscription</h1>
+
+<?php
+if (isset($_POST["frmInscription"])) {
+  $message = "je viens du formulaire";
+  dump($_POST);
+} else
+  $message = "je ne viens pas du formulaire";
+echo $message;
+
+?>
