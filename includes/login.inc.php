c3489d50afa05e2d1d4cb9fe819a2bc3741c7217
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-06-08 15:37:06 +0200

    login form

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,1 +1,34 @@
 <h1>Login</h1>
+
+<?php
+if (isset($_POST["frmLogin"])) {
+  $message = "je viens du formulaire";
+  $mail = htmlentities(trim($_POST['mail']));
+  $password = htmlentities($_POST['password']);
+
+  $erreurs = array();
+
+  $erreurs = checkErrors([$mail]);
+  if (mb_strlen($mail) === 0 || !filter_var($mail, FILTER_VALIDATE_EMAIL))
+    array_push($erreurs, "E-mail invalide.");
+  if (mb_strlen($password) === 0 || $password !== "toto")
+    array_push($erreurs, "Mot de passe incorrect.");
+
+  if (count($erreurs)) {
+    $messageErreur = "<ul>";
+    for ($i = 0; $i < count($erreurs); $i++) {
+      $messageErreur .= "<li>";
+      $messageErreur .= $erreurs[$i];
+      $messageErreur .= "</li>";
+    }
+    $messageErreur .= "</ul>";
+    echo $messageErreur;
+    include "./includes/frmLogin.php";
+  } else
+    displayMessage("Vous êtes connecté");
+} else {
+  $mail = "";
+  $message = "je ne viens pas du formulaire";
+  include "./includes/frmLogin.php";
+}
+displayMessage($message);
