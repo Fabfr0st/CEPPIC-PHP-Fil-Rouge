47ecd2b5cab463332e076c224ec6d85da9644ca5
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-06-08 15:03:22 +0200

    form and managing input errors

--- includes/frmInscription.php
+++ includes/frmInscription.php
@@ -1,15 +1,15 @@
 <form action="index.php?page=inscription" method="post">
     <div>
         <label for="nom">Nom : </label>
-        <input type="text" name="nom" id="nom">
+        <input type="text" name="nom" id="nom" value="<?= $nom ?>">
     </div>
     <div>
         <label for="prenom">Prénom : </label>
-        <input type="text" name="prenom" id="prenom">
+        <input type="text" name="prenom" id="prenom" value="<?= $prenom ?>">
     </div>
     <div>
         <label for="mail">Email : </label>
-        <input type="text" name="mail" id="mail">
+        <input type="text" name="mail" id="mail" value="<?= $mail ?>">
     </div>
     <div>
         <input type="reset" value="Effacer">
--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -3,9 +3,34 @@
 <?php
 if (isset($_POST["frmInscription"])) {
   $message = "je viens du formulaire";
-  dump($_POST);
-} else
-  $message = "je ne viens pas du formulaire";
-echo $message;
+  $nom = htmlentities(trim($_POST['nom']));
+  $prenom = htmlentities(trim($_POST['prenom']));
+  $mail = htmlentities(trim($_POST['mail']));
 
-?>
+  $erreurs = array();
+
+  if (mb_strlen($nom) === 0)
+    array_push($erreurs, "Il manque votre nom.");
+  if (mb_strlen($prenom) === 0)
+    array_push($erreurs, "Il manque votre prénom.");
+  if (mb_strlen($mail) === 0)
+    array_push($erreurs, "Il manque votre e-mail.");
+
+  if (count($erreurs)) {
+    $messageErreur = "<ul>";
+    for ($i = 0; $i < count($erreurs); $i++) {
+      $messageErreur .= "<li>";
+      $messageErreur .= $erreurs[$i];
+      $messageErreur .= "</li>";
+    }
+    $messageErreur .= "</ul>";
+    echo $messageErreur;
+    include "./includes/frmInscription.php";
+  } else
+    displayMessage("Pas d'erreur");
+} else {
+  $nom = $prenom = $mail = "";
+  $message = "je ne viens pas du formulaire";
+  include "./includes/frmInscription.php";
+}
+displayMessage($message);
