aef1680873adaeb16bbe62c5fe4acde6f55cf655
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-06-13 09:26:25 +0200

    adding passwords in inscription page

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -6,15 +6,26 @@ if (isset($_POST["frmInscription"])) {
   $nom = htmlentities(trim($_POST['nom']));
   $prenom = htmlentities(trim($_POST['prenom']));
   $mail = htmlentities(trim($_POST['mail']));
+  $password1 = htmlentities(trim($_POST['password1']));
+  $password2 = htmlentities(trim($_POST['password2']));
 
   $erreurs = array();
 
   if (mb_strlen($nom) === 0)
     array_push($erreurs, "Il manque votre nom.");
+
   if (mb_strlen($prenom) === 0)
     array_push($erreurs, "Il manque votre prénom.");
+
   if (mb_strlen($mail) === 0)
     array_push($erreurs, "Il manque votre e-mail.");
+  elseif (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+    array_push($erreurs, "Votre adresse mail n'est pas conforme");
+
+  if (mb_strlen($password1) === 0 || mb_strlen($password2) === 0)
+    array_push($erreurs, "Veuillez saisir votre mot de passe et sa confirmation");
+  elseif ($password1 !== $password2)
+    array_push($erreurs, "Vos mots de passe ne sont pas identiques");
 
   if (count($erreurs)) {
     $messageErreur = "<ul>";
@@ -23,14 +34,26 @@ if (isset($_POST["frmInscription"])) {
       $messageErreur .= $erreurs[$i];
       $messageErreur .= "</li>";
     }
+
     $messageErreur .= "</ul>";
     echo $messageErreur;
+
     include "./includes/frmInscription.php";
-  } else
-    displayMessage("Pas d'erreur");
+  } else {
+    $password = password_hash($password1, PASSWORD_DEFAULT);
+
+    $requete = "INSERT INTO utilisateurs (id_utilisateur, nom, prenom, mail, password)
+    VALUES (NULL, '$nom', '$prenom', '$mail', '$password');";
+
+    $queryInsert = new Sql();
+    $queryInsert->inserer($requete);
+
+
+    displayMessage("Requête OK");
+  }
 } else {
   $nom = $prenom = $mail = "";
-  $message = "je ne viens pas du formulaire";
+  //$message = "je ne viens pas du formulaire";
   include "./includes/frmInscription.php";
 }
 displayMessage($message);
