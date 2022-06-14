e926594ed565ab737049654c858149d2421f868f
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-06-14 09:43:48 +0200

    adding session management for login

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 spl_autoload_register(function ($className) {
     require './classes/' . $className . '.php';
@@ -11,3 +12,7 @@ date_default_timezone_set('Europe/Paris');
 require_once "./includes/head.php";
 require_once "./includes/main.php";
 require_once "./includes/footer.php";
+
+// $_SESSION['count'] = ($_SESSION['count'] ?? 0) + 1;
+
+// print_r($_SESSION);
