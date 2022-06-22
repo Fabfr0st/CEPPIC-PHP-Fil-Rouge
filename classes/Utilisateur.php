9354c392393c0739da2ae7e8303da9fa27f8d8ac
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-06-22 11:29:10 +0200

    user class implemeting user interface

---
+++ classes/Utilisateur.php
@@ -0,0 +1,24 @@
+<?php
+
+class Utilisateur implements Utilisateur
+{
+    public function inscrireUtilisateur()
+    {
+    }
+
+    public function modifierUtilisateur()
+    {
+    }
+
+    public function supprimerUtilisateur()
+    {
+    }
+
+    public function connecterUtilisateur()
+    {
+    }
+
+    public function deconnecterUtilisateur()
+    {
+    }
+}
